bfe31b6a055b16b4ce12d99c24ec40105cb2f529
xinyu <xinyu@163.com>
2021-01-15 17:19:02 +0800

    加了个感叹号！

--- src/main/java/com/newpro/demo/controller/Controller.java
+++ src/main/java/com/newpro/demo/controller/Controller.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @RequestMapping("")
       public String hello(){
-          return "hello springboot";
+          return "hello springboot！";
       }
 }
